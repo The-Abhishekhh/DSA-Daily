bd5bb8bbabc5047e18a68e41f649d6072e1d0589
theabhishekh <abhikumar1324@gmail.com>
2026-09-11 21:52:55 +0530

    Wildcard Matching leetcode 44 commit

---
+++ src/Arrays/NonStriverQuestions/Wildcard_Matching_44.java
@@ -0,0 +1,35 @@
+package Arrays.NonStriverQuestions;
+
+public class Wildcard_Matching_44 {
+    public boolean isMatch(String s, String p) {
+        int i = 0, j = 0;
+        int star = -1;
+        int match = 0;
+
+        while (i < s.length()) {
+            if (j < p.length() &&
+                    (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i))) {
+                i++;
+                j++;
+            }
+            else if (j < p.length() && p.charAt(j) == '*') {
+                star = j;
+                match = i;
+                j++;
+            }
+            else if (star != -1) {
+                j = star + 1;
+                i = ++match;
+            }
+            else {
+                return false;
+            }
+        }
+
+        while (j < p.length() && p.charAt(j) == '*') {
+            j++;
+        }
+
+        return j == p.length();
+    }
+}
